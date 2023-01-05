54d949da82579137d2ae3b247158578329405451
mishanianod <mishanian@orangedigital.com>
2023-01-05 14:09:12 -0500

    add  console log

--- CSFLEMongooseExample.js
+++ CSFLEMongooseExample.js
@@ -18,18 +18,29 @@ const keyVaultNamespace = 'client.encryption';
 const kmsProviders = { local: { key } };
 
 async function main() {
-  await mongoose.connect(
-    'mongodb+srv://testproduser:vJcgAmw1HD5KnNyh@employeedomaincluster.2l9uo.mongodb.net/?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Configure auto encryption
-      autoEncryption: {
-        keyVaultNamespace,
-        kmsProviders,
+  await mongoose
+    .connect(
+      'mongodb+srv://testproduser:vJcgAmw1HD5KnNyh@employeedomaincluster.2l9uo.mongodb.net/employeeDomain?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // Configure auto encryption
+        autoEncryption: {
+          keyVaultNamespace,
+          kmsProviders,
+        },
+      }
+    )
+    .then(
+      () => {
+        console.log(
+          `connected to MongoDB ${mongoose.connection.db.databaseName}`
+        );
       },
-    }
-  );
+      (err) => {
+        console.error({ message: err.message, metaData: err.stack });
+      }
+    );
 
   const encryption = new ClientEncryption(mongoose.connection.client, {
     keyVaultNamespace,
